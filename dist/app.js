a01e972fda87565feece5fad3e0f83b8e33ab893
cossack910 <maris160zhk@gmail.com>
2023-07-04 21:19:41 +0900

    ok

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello, world!");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild;
+        this.attach();
+    }
+    //appにテンプレートフォームをアタッチ
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prInput = new ProjectInput();
