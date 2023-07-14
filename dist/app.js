ffdeb6400da2708fbd18e3ac72e5cab2e6824135
cossack910 <maris160zhk@gmail.com>
2023-07-15 03:01:11 +0900

    a

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+function validate(validatableInput) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === "string") {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === "string") {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null &&
+        typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    return isValid;
+}
 //デコレータ
 function autobind(target, methosName, descriptor) {
     const originalMethod = descriptor.value;
@@ -36,9 +57,24 @@ class ProjectInput {
         const entererdDescription = this.descriptionInputElemnt.value;
         const entererdMandayInput = this.mandayInputElemnt.value;
         //バリデーション
-        if (entererdTitle.trim().length == 0 ||
-            entererdDescription.trim().length == 0 ||
-            entererdMandayInput.trim().length == 0) {
+        const titleValidatable = {
+            value: entererdTitle,
+            required: true,
+        };
+        const descriptionValidatable = {
+            value: entererdDescription,
+            required: true,
+            minLength: 5,
+        };
+        const mandayValidatable = {
+            value: +entererdMandayInput,
+            required: true,
+            min: 1,
+            max: 1000,
+        };
+        if (!validate(titleValidatable) &&
+            !validate(descriptionValidatable) &&
+            !validate(mandayValidatable)) {
             alert("入力間違い");
             return;
         }
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,45 @@
+//Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.maxLength;
+  }
+
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+
+  return isValid;
+}
+
 //デコレータ
 function autobind(
   target: any,
@@ -52,10 +94,25 @@ class ProjectInput {
     const entererdDescription = this.descriptionInputElemnt.value;
     const entererdMandayInput = this.mandayInputElemnt.value;
     //バリデーション
+    const titleValidatable: Validatable = {
+      value: entererdTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: entererdDescription,
+      required: true,
+      minLength: 5,
+    };
+    const mandayValidatable: Validatable = {
+      value: +entererdMandayInput,
+      required: true,
+      min: 1,
+      max: 1000,
+    };
     if (
-      entererdTitle.trim().length == 0 ||
-      entererdDescription.trim().length == 0 ||
-      entererdMandayInput.trim().length == 0
+      !validate(titleValidatable) &&
+      !validate(descriptionValidatable) &&
+      !validate(mandayValidatable)
     ) {
       alert("入力間違い");
       return;
@@ -63,6 +120,7 @@ class ProjectInput {
       return [entererdTitle, entererdDescription, +entererdMandayInput];
     }
   }
+
   //フォームクリア
   private clearInputs(): void {
     this.titleInputElemnt.value = "";
