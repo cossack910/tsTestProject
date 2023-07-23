4c31da5b31bbb32a00fa5d11f18eccf5ab00cc53
cossack910 <maris160zhk@gmail.com>
2023-07-23 19:09:33 +0900

    ゲッター

--- dist/app.js
+++ dist/app.js
@@ -103,6 +103,15 @@ class Component {
     }
 }
 class ProjectItem extends Component {
+    //getter
+    get manday() {
+        if (this.project.manday < 20) {
+            return this.project.manday.toString() + "人日";
+        }
+        else {
+            return (this.project.manday / 20).toString() + "人月";
+        }
+    }
     constructor(hostId, project) {
         super("single-project", hostId, false, project.id);
         this.project = project;
@@ -112,8 +121,7 @@ class ProjectItem extends Component {
     configure() { }
     renderContent() {
         this.element.querySelector("h2").textContent = this.project.title;
-        this.element.querySelector("h3").textContent =
-            this.project.manday.toString();
+        this.element.querySelector("h3").textContent = this.manday;
         this.element.querySelector("p").textContent = this.project.description;
     }
 }
