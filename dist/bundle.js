43903d2a71214669da40de9a6d9224710137270a
cossack910 <maris160zhk@gmail.com>
2023-07-27 06:48:26 +0900

    ファイル分割

--- dist/bundle.js
+++ dist/bundle.js
@@ -24,8 +24,7 @@ var App;
     }
     App.Project = Project;
 })(App || (App = {}));
-/// <reference path="drag-drop-interfaces.ts" />
-/// <reference path="project-model.ts" />
+//ファイル外で使うものだけをexportする
 var App;
 (function (App) {
     class State {
@@ -66,7 +65,11 @@ var App;
             }
         }
     }
-    const projectState = ProjectState.getInstance();
+    App.ProjectState = ProjectState;
+    App.projectState = ProjectState.getInstance();
+})(App || (App = {}));
+var App;
+(function (App) {
     function validate(validatableInput) {
         let isValid = true;
         if (validatableInput.required) {
@@ -93,6 +96,10 @@ var App;
         }
         return isValid;
     }
+    App.validate = validate;
+})(App || (App = {}));
+var App;
+(function (App) {
     // autobind decorator
     function autobind(_, _2, descriptor) {
         const originalMethod = descriptor.value;
@@ -105,6 +112,10 @@ var App;
         };
         return adjDescriptor;
     }
+    App.autobind = autobind;
+})(App || (App = {}));
+var App;
+(function (App) {
     // Component Class
     class Component {
         constructor(templateId, hostElementId, insertAtStart, newElementId) {
@@ -121,8 +132,158 @@ var App;
             this.hostElement.insertAdjacentElement(insertAtBeginning ? "afterbegin" : "beforeend", this.element);
         }
     }
+    App.Component = Component;
+})(App || (App = {}));
+/// <reference path="./base-component.ts" />
+var App;
+(function (App) {
+    // ProjectInput Class
+    class ProjectInput extends App.Component {
+        constructor() {
+            super("project-input", "app", true, "user-input");
+            this.titleInputElement = this.element.querySelector("#title");
+            this.descriptionInputElement = this.element.querySelector("#description");
+            this.mandayInputElement = this.element.querySelector("#manday");
+            this.configure();
+        }
+        configure() {
+            this.element.addEventListener("submit", this.submitHandler);
+        }
+        renderContent() { }
+        gatherUserInput() {
+            const enteredTitle = this.titleInputElement.value;
+            const enteredDescription = this.descriptionInputElement.value;
+            const enteredManday = this.mandayInputElement.value;
+            const titleValidatable = {
+                value: enteredTitle,
+                required: true,
+            };
+            const descriptionValidatable = {
+                value: enteredDescription,
+                required: true,
+                minLength: 5,
+            };
+            const mandayValidatable = {
+                value: +enteredManday,
+                required: true,
+                min: 1,
+                max: 1000,
+            };
+            if (!App.validate(titleValidatable) ||
+                !App.validate(descriptionValidatable) ||
+                !App.validate(mandayValidatable)) {
+                alert("入力値が正しくありません。再度お試しください。");
+                return;
+            }
+            else {
+                return [enteredTitle, enteredDescription, +enteredManday];
+            }
+        }
+        clearInputs() {
+            this.titleInputElement.value = "";
+            this.descriptionInputElement.value = "";
+            this.mandayInputElement.value = "";
+        }
+        submitHandler(event) {
+            event.preventDefault();
+            const userInput = this.gatherUserInput();
+            if (Array.isArray(userInput)) {
+                const [title, desc, manday] = userInput;
+                App.projectState.addProject(title, desc, manday);
+                this.clearInputs();
+            }
+        }
+    }
+    __decorate([
+        App.autobind
+    ], ProjectInput.prototype, "submitHandler", null);
+    App.ProjectInput = ProjectInput;
+})(App || (App = {}));
+/// <reference path="./base-component.ts" />
+var App;
+(function (App) {
+    // ProjectList Class
+    class ProjectList extends App.Component {
+        constructor(type) {
+            super("project-list", "app", false, `${type}-projects`);
+            this.type = type;
+            this.assignedProjects = [];
+            this.configure();
+            this.renderContent();
+        }
+        dragOverHandler(event) {
+            if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+                event.preventDefault();
+                const listEl = this.element.querySelector("ul");
+                listEl.classList.add("droppable");
+            }
+        }
+        dropHandler(event) {
+            const prjId = event.dataTransfer.getData("text/plain");
+            App.projectState.moveProject(prjId, this.type === "active" ? App.ProjectStatus.Active : App.ProjectStatus.Finished);
+        }
+        dragLeaveHandler(_) {
+            const listEl = this.element.querySelector("ul");
+            listEl.classList.remove("droppable");
+        }
+        configure() {
+            this.element.addEventListener("dragover", this.dragOverHandler);
+            this.element.addEventListener("drop", this.dropHandler);
+            this.element.addEventListener("dragleave", this.dragLeaveHandler);
+            App.projectState.addListener((projects) => {
+                const relevantProjects = projects.filter((prj) => {
+                    if (this.type === "active") {
+                        return prj.status === App.ProjectStatus.Active;
+                    }
+                    return prj.status === App.ProjectStatus.Finished;
+                });
+                this.assignedProjects = relevantProjects;
+                this.renderProjects();
+            });
+        }
+        renderContent() {
+            const listId = `${this.type}-projects-list`;
+            this.element.querySelector("ul").id = listId;
+            this.element.querySelector("h2").textContent =
+                this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
+        }
+        renderProjects() {
+            const listEl = document.getElementById(`${this.type}-projects-list`);
+            listEl.innerHTML = "";
+            for (const prjItem of this.assignedProjects) {
+                new App.ProjectItem(listEl.id, prjItem);
+            }
+        }
+    }
+    __decorate([
+        App.autobind
+    ], ProjectList.prototype, "dragOverHandler", null);
+    __decorate([
+        App.autobind
+    ], ProjectList.prototype, "dropHandler", null);
+    __decorate([
+        App.autobind
+    ], ProjectList.prototype, "dragLeaveHandler", null);
+    App.ProjectList = ProjectList;
+})(App || (App = {}));
+/// <reference path="./models/drag-drop.ts" />
+/// <reference path="./models/project.ts" />
+/// <reference path="./state/project.ts" />
+/// <reference path="./util/validation.ts" />
+/// <reference path="./decoraters/autobind.ts" />
+/// <reference path="./components/project-input.ts" />
+/// <reference path="./components/project-list.ts" />
+var App;
+(function (App) {
+    const prjInput = new App.ProjectInput();
+    const activePrjList = new App.ProjectList("active");
+    const finishedPrjList = new App.ProjectList("finished");
+})(App || (App = {}));
+/// <reference path="./base-component.ts" />
+var App;
+(function (App) {
     // ProjectItem Class
-    class ProjectItem extends Component {
+    class ProjectItem extends App.Component {
         get manday() {
             if (this.project.manday < 20) {
                 return this.project.manday.toString() + "人日";
@@ -155,131 +316,7 @@ var App;
         }
     }
     __decorate([
-        autobind
+        App.autobind
     ], ProjectItem.prototype, "dragStartHandler", null);
-    // ProjectList Class
-    class ProjectList extends Component {
-        constructor(type) {
-            super("project-list", "app", false, `${type}-projects`);
-            this.type = type;
-            this.assignedProjects = [];
-            this.configure();
-            this.renderContent();
-        }
-        dragOverHandler(event) {
-            if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
-                event.preventDefault();
-                const listEl = this.element.querySelector("ul");
-                listEl.classList.add("droppable");
-            }
-        }
-        dropHandler(event) {
-            const prjId = event.dataTransfer.getData("text/plain");
-            projectState.moveProject(prjId, this.type === "active" ? App.ProjectStatus.Active : App.ProjectStatus.Finished);
-        }
-        dragLeaveHandler(_) {
-            const listEl = this.element.querySelector("ul");
-            listEl.classList.remove("droppable");
-        }
-        configure() {
-            this.element.addEventListener("dragover", this.dragOverHandler);
-            this.element.addEventListener("drop", this.dropHandler);
-            this.element.addEventListener("dragleave", this.dragLeaveHandler);
-            projectState.addListener((projects) => {
-                const relevantProjects = projects.filter((prj) => {
-                    if (this.type === "active") {
-                        return prj.status === App.ProjectStatus.Active;
-                    }
-                    return prj.status === App.ProjectStatus.Finished;
-                });
-                this.assignedProjects = relevantProjects;
-                this.renderProjects();
-            });
-        }
-        renderContent() {
-            const listId = `${this.type}-projects-list`;
-            this.element.querySelector("ul").id = listId;
-            this.element.querySelector("h2").textContent =
-                this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
-        }
-        renderProjects() {
-            const listEl = document.getElementById(`${this.type}-projects-list`);
-            listEl.innerHTML = "";
-            for (const prjItem of this.assignedProjects) {
-                new ProjectItem(listEl.id, prjItem);
-            }
-        }
-    }
-    __decorate([
-        autobind
-    ], ProjectList.prototype, "dragOverHandler", null);
-    __decorate([
-        autobind
-    ], ProjectList.prototype, "dropHandler", null);
-    __decorate([
-        autobind
-    ], ProjectList.prototype, "dragLeaveHandler", null);
-    // ProjectInput Class
-    class ProjectInput extends Component {
-        constructor() {
-            super("project-input", "app", true, "user-input");
-            this.titleInputElement = this.element.querySelector("#title");
-            this.descriptionInputElement = this.element.querySelector("#description");
-            this.mandayInputElement = this.element.querySelector("#manday");
-            this.configure();
-        }
-        configure() {
-            this.element.addEventListener("submit", this.submitHandler);
-        }
-        renderContent() { }
-        gatherUserInput() {
-            const enteredTitle = this.titleInputElement.value;
-            const enteredDescription = this.descriptionInputElement.value;
-            const enteredManday = this.mandayInputElement.value;
-            const titleValidatable = {
-                value: enteredTitle,
-                required: true,
-            };
-            const descriptionValidatable = {
-                value: enteredDescription,
-                required: true,
-                minLength: 5,
-            };
-            const mandayValidatable = {
-                value: +enteredManday,
-                required: true,
-                min: 1,
-                max: 1000,
-            };
-            if (!validate(titleValidatable) ||
-                !validate(descriptionValidatable) ||
-                !validate(mandayValidatable)) {
-                alert("入力値が正しくありません。再度お試しください。");
-                return;
-            }
-            else {
-                return [enteredTitle, enteredDescription, +enteredManday];
-            }
-        }
-        clearInputs() {
-            this.titleInputElement.value = "";
-            this.descriptionInputElement.value = "";
-            this.mandayInputElement.value = "";
-        }
-        submitHandler(event) {
-            event.preventDefault();
-            const userInput = this.gatherUserInput();
-            if (Array.isArray(userInput)) {
-                const [title, desc, manday] = userInput;
-                projectState.addProject(title, desc, manday);
-                this.clearInputs();
-            }
-        }
-    }
-    __decorate([
-        autobind
-    ], ProjectInput.prototype, "submitHandler", null);
-    const prjInput = new ProjectInput();
-    const activePrjList = new ProjectList("active");
-    const finishedPrjList = new ProjectList("finished");
+    App.ProjectItem = ProjectItem;
 })(App || (App = {}));
--- src/README.md
+++ src/README.md
@@ -1,9 +1,10 @@
 # memo
 
-| No                                                         | title                          |            |
-| ---------------------------------------------------------- | ------------------------------ | ---------- |
-| [#1](https://github.com/cossack910/tsTestProject/issues/1) | event.preventDefault();        | javascript |
-| [#2](https://github.com/cossack910/tsTestProject/issues/2) | insertAdjacentElement          | javascript |
-| [#3](https://github.com/cossack910/tsTestProject/issues/3) | シングルトン                   | typescript |
-| [#4](https://github.com/cossack910/tsTestProject/issues/4) | event.dataTransfer             | javascript |
-| [#5](https://github.com/cossack910/tsTestProject/issues/5) | トリプルスラシュディレクティブ | typescript |
+| No                                                         | title                            |            |
+| ---------------------------------------------------------- | -------------------------------- | ---------- |
+| [#1](https://github.com/cossack910/tsTestProject/issues/1) | event.preventDefault();          | javascript |
+| [#2](https://github.com/cossack910/tsTestProject/issues/2) | insertAdjacentElement            | javascript |
+| [#3](https://github.com/cossack910/tsTestProject/issues/3) | シングルトン                     | typescript |
+| [#4](https://github.com/cossack910/tsTestProject/issues/4) | event.dataTransfer               | javascript |
+| [#5](https://github.com/cossack910/tsTestProject/issues/5) | トリプルスラシュディレクティブ   | typescript |
+| [#6](https://github.com/cossack910/tsTestProject/issues/6) | tsconfig.json outFIle オプション | typescript |
